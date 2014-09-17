712e6f53c1c5843ee22074ce98e978d61fc4cab7
mserino <m.serino@hotmail.it>
2014-09-17 16:00:33 +0100

    Machine returns a product when selected and no money if the money provided is correct - it returns change if too much money is provided

--- lib/machine.rb
+++ lib/machine.rb
@@ -1,29 +1,57 @@
 class Machine
 
 	def products
-		{"Coke" => 2,
-		 "Mars" => 3,
-		 "Pringles" => 4}
+		[
+			{product: {"Coke" => 150.0}, quantity: nil},
+			{product: {"Mars" => 50.0}, quantity: nil},
+			{product: {"Pringles" => 175.0}, quantity: nil}
+		]
+	end
+
+	def products_names
+		products.map{|p| p[:product].keys}.flatten
 	end
 
 	def change
 		{
-			"1p" => nil,
-			"2p" => nil,
-			"5p" => nil,
-			"10p" => nil,
-			"20p" => nil,
-			"50p" => nil,
-			"£1" => nil,
-			"£2" => nil
+			"1p" => 10,
+			"2p" => 10,
+			"5p" => 10,
+			"10p" => 10,
+			"20p" => 8,
+			"50p" => 8,
+			"£1" => 5,
+			"£2" => 5
 		}
 	end
 
 	def price(product)
-		products[product]
+		p = products.select{|p| p[:product][product]}
+		p[0][:product][product]
 	end
 
-	def select(product)
-		return product
+	def convert(price)
+		if price.class == Fixnum || price.class == Float
+			return "#{price.to_i}p" if price < 100
+			return "£#{price.to_i/100}" if price >= 100
+		end
+
+		if price.class == String
+			return price.to_f if price.include? "p"
+			return (price.delete("£").to_f)*100 if price.include? "£"
+		end
 	end
+
+	def select(product, price)
+		new_price = convert(price)
+		if new_price == price(product)
+			return "Your product: " + product
+		end
+
+		if new_price > price(product)
+			change = convert(new_price - price(product))
+			return "Your product: #{product} - Change: #{change}"
+		end
+	end
+
 end
--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -4,19 +4,71 @@ describe Machine do
 	let(:machine) { Machine.new }
 
 	it 'contains products when created' do
-		expect(machine.products.keys).to eq ["Coke", "Mars", "Pringles"]
+		expect(machine.products_names).to eq ["Coke", "Mars", "Pringles"]
 	end
 
 	it 'products have a price' do
-		expect(machine.price("Coke")).to eq 2
+		expect(machine.price("Coke")).to eq 150
 	end
 
 	it 'contains change when created' do
 		expect(machine.change.keys).to eq ["1p", "2p", "5p", "10p", "20p", "50p", "£1", "£2"]
 	end
 
-	it 'returns a product when selected' do
-		expect(machine.select("Coke")).to eq "Coke"
+	it 'contains 10 coins of denomination 1p' do
+		expect(machine.change["1p"]).to eq 10
+	end
+
+	it 'contains 10 coins of denomination 2p' do
+		expect(machine.change["2p"]).to eq 10
+	end
+
+	it 'contains 10 coins of denomination 5p' do
+		expect(machine.change["5p"]).to eq 10
+	end
+
+	it 'contains 10 coins of denomination 10p' do
+		expect(machine.change["10p"]).to eq 10
+	end
+
+	it 'contains 8 coins of denomination 20p' do
+		expect(machine.change["20p"]).to eq 8
+	end
+
+	it 'contains 8 coins of denomination 50p' do
+		expect(machine.change["50p"]).to eq 8
+	end
+
+	it 'contains 5 coins of denomination £1' do
+		expect(machine.change["£1"]).to eq 5
+	end
+
+	it 'contains 5 coins of denomination £2' do
+		expect(machine.change["£2"]).to eq 5
+	end
+
+	it 'can convert price to denomination p' do
+		expect(machine.convert(20)).to eq "20p"
+	end
+
+	it 'can convert price to denomination £' do
+		expect(machine.convert(100)).to eq "£1"
+	end
+
+	it 'can convert denomination p to price' do
+		expect(machine.convert("20p")).to eq 20
+	end
+
+	it 'can convert denomination £ to price' do
+		expect(machine.convert("£1")).to eq 100
+	end
+
+	it 'returns a product when selected and no money if the money provided is correct' do
+		expect(machine.select("Coke", "£1.5")).to eq "Your product: Coke"
+	end
+
+	it 'returns change if too much money is provided' do
+		expect(machine.select("Coke", "£2")).to eq  "Your product: Coke - Change: 50p"
 	end
 
 end
